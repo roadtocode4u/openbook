8f9cc1cf4744ae9564f4b5af8bc4212ff8473085
Vaibhavihole31 <itspinki05@gmail.com>
2022-10-05 14:43:42 +0530

    feat: added signup api

--- server/models/User.js
+++ server/models/User.js
@@ -1,14 +1,15 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    fullName: { type: String },
-    email: { type: String },
-    password: { type: String },
-    mobile: { type: String }
-  },
+  fullName: { type: String, required: [true, 'fullName cannot be empty'] },
+  email: { type: String, required: [true, 'email cannot be empty'] },
+  password: { type: String, required: [true, 'password cannot be empty'] },
+  mobile: { type: String, required: [true, 'mobile cannot be empty'] }
+},
   {
     timestamps: true
   }
-  )
-  
-  module.exports = mongoose.model('User', userSchema)
+)
+
+const User = mongoose.model('User', userSchema);
+export default User;
