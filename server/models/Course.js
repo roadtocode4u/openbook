f51ddde63b9227866d8fbc5e669f971324d68c7b
Vaibhavihole31 <itspinki05@gmail.com>
2022-10-12 13:38:31 +0530

    ref: add params in course model

--- server/models/Course.js
+++ server/models/Course.js
@@ -2,7 +2,10 @@ import mongoose from "mongoose";
 
 const courseSchema = new mongoose.Schema({
   courseName: { type: String, required: [true, 'courseName cannot be empty'] },
-  courseCode: { type: String, required: [true, 'courseCode cannot be empty'] }
+  courseCode: { type: String, required: [true, 'courseCode cannot be empty'] },
+  branch: {type: String, required: [true, 'branch cannot be empty'] },
+  credit: {type: String, required: [type, 'credit cannot be empty'] },
+  thumbnail: {type: String, required: [type, 'thumbnail cannot be empty']}
 },
   {
     timestamps: true
