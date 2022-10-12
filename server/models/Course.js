deb17e8fed84629d41d659436114f46f5e97d7af
Vaibhavihole31 <itspinki05@gmail.com>
2022-10-12 14:21:28 +0530

    ref: rename params name in course model

--- server/models/Course.js
+++ server/models/Course.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
 
 const courseSchema = new mongoose.Schema({
-  courseName: { type: String, required: [true, 'courseName cannot be empty'] },
-  courseCode: { type: String, required: [true, 'courseCode cannot be empty'] },
-  branch: {type: String, required: [true, 'branch cannot be empty'] },
-  credit: {type: String, required: [type, 'credit cannot be empty'] },
-  thumbnail: {type: String, required: [type, 'thumbnail cannot be empty']}
+  title: { type: String, required: [true, 'title cannot be empty'] },
+  code: { type: String, required: [true, 'code cannot be empty'] },
+  branch: { type: String, required: [true, 'branch cannot be empty'] },
+  credits: { type: String, required: [type, 'credits cannot be empty'] },
+  thumbnail: { type: String, required: [type, 'thumbnail cannot be empty'] }
 },
   {
     timestamps: true
