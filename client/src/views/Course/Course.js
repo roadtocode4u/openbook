8e4f50c4c23d0a723a6728923577dea235f0d1a7
Suraj Shende <surajshende247@gmail.com>
2022-10-15 09:44:27 +0530

    feat: course material view added

---
+++ client/src/views/Course/Course.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import { useSearchParams } from 'react-router-dom'
+
+import "./Course.css";
+import StudyMaterialCard from '../../components/StudyMaterialCard/StudyMaterialCard';
+
+function Course() {
+  const [searchParams] = useSearchParams();
+  const courseCode = searchParams.get("courseCode");
+
+  const studyMaterials = [
+    {
+      "title": "Study Material 1",
+      "description": "This is first clss",
+      "url": "12345",
+      "contenttype": "video"
+    },
+    {
+      "title": "Study Material 2",
+      "description": "This is first clss",
+      "url": "12345",
+      "contenttype": "link"
+    },
+    {
+      "title": "Study Material 3",
+      "description": "This is first clss",
+      "url": "12345",
+      "contenttype": "pdf"
+    },
+    {
+      "title": "Study Material 4",
+      "description": "This is first clss",
+      "url": "12345",
+      "contenttype": "video"
+    },
+    {
+      "title": "Study Material 5",
+      "description": "This is first clss",
+      "url": "12345",
+      "contenttype": "pdf"
+    }
+  ];
+
+  return (
+    <div>
+      <div className='course-header'>
+        <h1>{courseCode}</h1>
+      </div>
+      <div className='course-material-container'>
+        <div className='row'>
+          {
+            studyMaterials.map((material, index) => {
+              return (
+                <div className='col-md-4 col-lg-3 d-flex justify-content-center' key={index}>
+                  <StudyMaterialCard material={material} />
+                </div>)
+            })
+          }
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Course
