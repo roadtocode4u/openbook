6f1e7aa3c36842cfadde8b37cdc1694510002d02
Vaibhavihole31 <itspinki05@gmail.com>
2022-11-22 10:41:21 +0530

    ref: subject card design

--- client/src/components/SubjectCard/SubjectCard.js
+++ client/src/components/SubjectCard/SubjectCard.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import './SubjectCard.css'
+import IcoCardOpen from './card-open-ico.png';
 
 function SubjectCard({subject}) {
   return (
     <>
-    <div className='subject-card text-center'>
-          <h1>{subject}</h1>
+    <div className='subject-card'>
+          <h5>{subject}</h5>
+          <img src={IcoCardOpen}
+        className="study-material-card-open-ico " />
     </div>
     </>
   )
