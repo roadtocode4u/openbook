bb573b48799662c3edddea209a322b8e319782da
Vaibhavihole31 <itspinki05@gmail.com>
2022-12-02 19:08:15 +0530

    feat: added subject mech branch

--- client/src/App.js
+++ client/src/App.js
@@ -21,13 +21,10 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path='/signup' element={<Signup />} />
         <Route path='/login' element={<Login />} />
-<<<<<<< HEAD
         <Route path="/course" element={<Course />} />
         <Route path="/studymaterial-add" element={<StudyMaterial/>} />
-=======
         <Route path="/course-view" element={<Course />} />
         <Route path="/add-study-material" element={<StudyMaterial/>} />
->>>>>>> e6cb068ba4108f904da3257df457356086f972c7
       </Routes>
       <Footer />
     </BrowserRouter>
