90f3f80c1b5af2609bde6ce5b3806b83484cf051
Vaibhavihole31 <itspinki05@gmail.com>
2023-03-12 08:37:42 +0530

    responder added in health controller

--- server/controllers/health.js
+++ server/controllers/health.js
@@ -1,7 +1,3 @@
 export const health = (req, res) => {
-    res.json({
-        status: 'ok',
-        message: 'Server is running'
-    })
-    responder(res, course, "course featch successfully!!");
+    responder(res, null, 'Server is running');
 }
